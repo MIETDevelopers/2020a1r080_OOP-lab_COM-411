0f2454f233d1930999b5d0d8905caba0a4f20ec3
Parmeet Kour <75742485+handy08@users.noreply.github.com>
2022-06-13 01:33:05 +0530

    To update and delete the details of a course using a member function of course class.

---
+++ activity3.cpp
@@ -0,0 +1,59 @@
+//To update and delete the details of a student using a member function of student class. 
+#include<iostream>
+using namespace std;
+class student
+{
+    int rno,age,pincode;
+    string name, dept,city;
+    public:
+    student(string n,int r,string d,int a,string c,int p)
+    {
+        name=n;
+        rno=r;
+        dept=d;
+        age=a;
+        city=c;
+        pincode=p;
+    }
+    void display()
+    {
+        cout<<"\nStudent Name: "<<name<<endl;
+        cout<<"Student Roll No: "<<rno<<endl;
+        cout<<"Student Department: "<<dept<<endl;
+        cout<<"Age: "<<age<<endl;
+        cout<<"City: "<<city<<endl;
+        cout<<"Pincode: "<<pincode<<endl;
+        cout<<endl;
+    }
+    void update()
+    {
+        cout<<"\nEnter Student Name: ";
+        cin>>name;
+        cout<<"Enter Student Roll No: ";
+        cin>>rno;
+        cout<<"Enter Student Department: ";
+        cin>>dept;
+        cout<<"Enter Age: ";
+        cin>>age;
+        cout<<"Enter City: ";
+        cin>>city;
+        cout<<"Enter Pincode: ";
+        cin>>pincode;
+    }
+    void del()
+    {
+        cout<<"\nDetails Deleted";
+    }
+    ~student(){}
+};
+int main()
+{
+    student s("Parmeet",88,"CSE",18,"Jammu",181152);
+    s.display();
+    cout<<"Updating the Details";
+    s.update();
+    cout<<"Details Updated";
+    cout<<"\n\nDeleting the Details";
+    s.del();
+    return 0;
+}       
